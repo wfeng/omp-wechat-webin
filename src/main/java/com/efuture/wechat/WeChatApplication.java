b2f1258073653e443abe5404b7f1aba200d3146f
王峰 <wangfeng@e-future.com.cn>
2021-08-12 10:46:29 +0800

    Lombok插件

--- src/main/java/com/efuture/wechat/WeChatApplication.java
+++ src/main/java/com/efuture/wechat/WeChatApplication.java
@@ -1,12 +1,15 @@
 package com.efuture.wechat;
 
 import com.efuture.wechat.common.SpringContext;
+import groovy.util.logging.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class WeChatApplication {
 
     public static void main(String[] args) {
+        log.info("启动服务");
         SpringContext.run(WeChatApplication.class, args);
     }
 
